4bffdf08f5549046f45320854f23506eeadb08be
Sven Jörns <github@sven-joerns.de>
2016-11-20 00:05:38 +0100

    whitespace

--- runpdfsam.sh
+++ runpdfsam.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
+# refresh sources
+git fetch upstream
+git checkout master
+git merge upstream/master
+
 # running PdfSam 
-RUNJAR=pdfsam-community-3.0.0.RELEASE-SNAPSHOT.jar
+RUNJAR=pdfsam-community-3.1.1.RELEASE-SNAPSHOT.jar
 if [ -f ./pdfsam-community/target/$RUNJAR ] 
 then
-	java -jar ./pdfsam-community/target/$RUNJAR
+        cd ./pdfsam-community/
+        mvn exec:java
+	#java -jar ./pdfsam-community/target/$RUNJAR
 else
-	mvn clean install -Dmaven.test.skip=true -Prelease
+	#mvn clean install -Dmaven.test.skip=true -Prelease
+	mvn clean install 
 	if [ -f ./pdfsam-community/target/$RUNJAR ] 
 	then
-		java -jar ./pdfsam-community/target/$RUNJAR
+		cd ./pdfsam-community/ 
+		mvn exec:java
+		#java -jar ./pdfsam-community/target/$RUNJAR
 	else
 		echo "Can't start PdfSam"
 		exit 1
