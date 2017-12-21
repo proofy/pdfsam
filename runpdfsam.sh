b06de9ca6e05011812f2a2177855c254e1b45b37
Sven Jörns <github@sven-joerns.de>
2017-12-21 10:49:49 +0100

    3.3.6

--- runpdfsam.sh
+++ runpdfsam.sh
@@ -5,7 +5,7 @@ git checkout master
 git merge upstream/master
 
 # running PdfSam 
-RUNJAR=pdfsam-community-3.3.3.RELEASE-SNAPSHOT.jar
+RUNJAR=pdfsam-community-3.3.6.RELEASE-SNAPSHOT.jar
 if [ -f ./pdfsam-community/target/$RUNJAR ] 
 then
         cd ./pdfsam-community/
